4dae8237a900698625e2d60d9a00db65c3a2a591
UltraNano09 <louis.sp.wagner@gmail.com>
2018-04-22 13:57:08 +0200

    a plus de pions ^^

--- JetPack.py
+++ JetPack.py
@@ -11,16 +11,6 @@ def Jetpack_off(event) :
 
 
 
-
-def pions(x, y, color):
-    r = 15
-
-    x = 2.5 + marge + pas_1 * (x - 1)
-    y = marge - 3 + pas_2 * (y - 1)
-
-    return Can.create_oval(x - r, y - r, x + r, y + r, fill = color ,outline = 'black')
-
-
 def zombie(x, y):
 
     x = 2.5 + marge + pas_1 * (x - 1)
@@ -44,17 +34,13 @@ pas_1 = cote_1 / nb_de_cases
 pas_2 = cote_2 / 8
 
 for i in range(0,nb_de_cases+1):
-    Can.create_line( 0, pas_1 * i + marge, cote_2 , pas_1 * i + marge, fill = 'black')  # Horizontales
-    Can.create_line(pas_2 + marge, cote_1 , pas_2 + marge, 0 , fill = 'black') # Verticales
+    Can.create_line( 0, pas_1 * i + marge, cote_2 , pas_1 * i + marge, fill = 'white')  # Horizontales
+    Can.create_line(pas_2 + marge, cote_1 , pas_2 + marge, 0 , fill = 'white') # Verticales
 
 fen.bind("<space>", Jetpack_on)
 fen.bind("<KeyRelease>", Jetpack_off)
 
 
-
-##Can.create_image(50 , 50, image = personnage)
-
-##Pion_1 = pions(3,2, 'gold' )
 Zombie1 = zombie(pas_1 ,3)
 
 Can.pack()
